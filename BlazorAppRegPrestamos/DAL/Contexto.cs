3fae7889bd8862e29efaf1c89262b00e6fac8b8a
Enyer De Jesus Holguin Toribio <enyer_holguin@ucne.edu.do>
2021-02-19 11:35:10 -0400

    Add project files.

---
+++ BlazorAppRegPrestamos/DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using BlazorAppRegPrestamos.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorAppRegPrestamos.DAL
+{
+    public class Contexto : DbContext 
+    {
+        public DbSet<Moras> Moras { get; set; }
+        public DbSet<Personas> Personas { get; set; }
+        public DbSet<Prestamos> Prestamos { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    }
+}
---
+++ BlazorAppRegPrestamos/Models/MorasDetalle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorAppRegPrestamos.Models
+{
+    public class MorasDetalle
+    {
+        [Key]
+        public int Id { get; set; }
+        public int MoraId { get; set; }
+        public int PrestamoId { get; set; }
+        public double Valor { get; set; }
+    }
+}
